83277f3448d6ec29972f03c175885553e402e99a
off-by-0point5 <61155885+off-by-0point5@users.noreply.github.com>
2021-08-16 20:39:11 +0200

    Create alpine-velocityproxy.sh

---
+++ alpine-velocityproxy.sh
@@ -0,0 +1,5 @@
+wget -O - https://raw.githubusercontent.com/off-by-0point5/console-scripts/main/alpine-setup.sh | sh
+apk add openjdk11-jre-headless
+wget -O proxy.jar https://ci.velocitypowered.com/job/velocity-3.0.0/37/artifact/proxy/build/libs/velocity-proxy-3.0.0-all.jar
+echo "java -Xms215M -Xmx1750M -jar proxy.jar" >> start.sh
+chmod u+x start.sh
