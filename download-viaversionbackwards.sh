37cdfa0aaae24469f2a9abea2eedf6cbb210b978
off-by-0point5 <61155885+off-by-0point5@users.noreply.github.com>
2021-08-17 17:20:20 +0200

    Create download-viaversionbackwards.sh

---
+++ download-viaversionbackwards.sh
@@ -0,0 +1,1 @@
+wget https://ci.viaversion.com/job/ViaVersion/530/artifact/build/libs/ViaVersion-4.0.1.jar https://ci.viaversion.com/view/ViaBackwards/job/ViaBackwards/221/artifact/build/libs/ViaBackwards-4.0.1.jar
