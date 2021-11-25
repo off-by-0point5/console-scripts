77ccbc520ae76e6974c29b0d062ca27ffe0d546e
off-by-0point5 <61155885+off-by-0point5@users.noreply.github.com>
2021-11-25 17:52:22 +0100

    Install mosh

--- alpine-setup.sh
+++ alpine-setup.sh
@@ -3,7 +3,7 @@ apk add openssh-server
 rc-update add sshd
 /etc/init.d/sshd start
 
-apk add rsync screen nano
+apk add rsync screen nano mosh
 echo "export TERM=vt100" >> .profile
 echo "export EDITOR=nano" >> .profile
 source .profile
