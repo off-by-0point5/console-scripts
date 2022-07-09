69270d21fafccfc07d723fbc079f7814d29863c4
offbyone <offbyone@disroot.org>
2022-07-09 07:26:47 +0200

    Add useful aliases (and install bat and exa)

--- alpine-setup.sh
+++ alpine-setup.sh
@@ -3,10 +3,11 @@ apk add openssh-server
 rc-update add sshd
 /etc/init.d/sshd start
 
-apk add rsync tmux micro mosh
+apk add rsync tmux micro mosh exa bat
 echo "export TERM=vt100" >> .profile
 echo "export EDITOR=micro" >> .profile
 echo "alias here=\"cd \$(pwd)\"" >> .profile
 echo "alias copy=\"rsync -a\"" >> .profile
 echo "alias lsl=\"exa -l -a\"" >> .profile
+echo "alias cat=\"bat\"" >> .profile
 source .profile
